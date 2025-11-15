eb0c417de5cc180f184fb4f884da1f75fc9133dc
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-11-15 13:05:08 +0100

    Day 4 and Day 5

---
+++ kotlin/2015/src/Day04.kt
@@ -0,0 +1,28 @@
+import java.io.File
+import java.security.MessageDigest
+
+fun main() {
+    val text = File("2015/input/day04.txt").readText()
+    val md = MessageDigest.getInstance("MD5")
+
+    var i = 1
+    while (true) {
+        val digest = md.digest("$text$i".toByteArray())
+        val hex = digest.toHexString()
+        if (hex.startsWith("00000")) {
+            println("Part 1: $i")
+            break
+        }
+        i++
+    }
+
+    while (true) {
+        val digest = md.digest("$text$i".toByteArray())
+        val hex = digest.toHexString()
+        if (hex.startsWith("000000")) {
+            println("Part 2: $i")
+            break
+        }
+        i++
+    }
+}
